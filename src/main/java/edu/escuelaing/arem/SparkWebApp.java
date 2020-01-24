4a428c459e7babe5156168efa6252f813debcbd8
David Caycedo <david.caycedo@mail.escuelaing.edu.co>
2020-01-24 12:41:33 -0500

    proyecto completo

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -11,6 +11,7 @@ import static spark.Spark.*;
  */
 public class SparkWebApp {
     public static void main(String[] args) { 
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
     static int getPort() {  
