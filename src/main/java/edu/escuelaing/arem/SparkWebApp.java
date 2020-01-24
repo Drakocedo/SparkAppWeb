2fe245748cb1797914cd2ca2d74e00fe58260555
David Caycedo <david.caycedo@mail.escuelaing.edu.co>
2020-01-24 12:38:49 -0500

    procfile

---
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+import static spark.Spark.*;
+/**
+ *
+ * @author david.caycedo
+ */
+public class SparkWebApp {
+    public static void main(String[] args) { 
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+    static int getPort() {  
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+            
+          }        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)    }
+}
+}
