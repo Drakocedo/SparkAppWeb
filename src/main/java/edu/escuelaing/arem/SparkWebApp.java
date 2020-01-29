a9063a0a4e0282d0c1ce78677b6f7b7bdec30cd8
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-01-29 12:49:08 -0500

    par

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -12,7 +12,34 @@ import static spark.Spark.*;
 public class SparkWebApp {
     public static void main(String[] args) { 
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroko");
+        get("/hello", (req, res) -> "Hello Heroko <br>  <br> Digite los numeros separados por comas: <br>  <input type=\"text\" name=\"fname\"><br>  <input type=\"button\" value=\"Click me\">");
+    }
+    
+    
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>HTML Forms</h2>"
+                + "<form action=\"/results\">"
+                + "  First name:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
+                + "  <br>"
+                + "  Last name:<br>"
+                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
     }
     static int getPort() {  
         if (System.getenv("PORT") != null) {
