e62a77ba1c4690db993fdbca33b647e908b0180c
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-01-29 10:49:42 -0500

    test

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -12,7 +12,7 @@ import static spark.Spark.*;
 public class SparkWebApp {
     public static void main(String[] args) { 
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        get("/hello", (req, res) -> "Hello Heroko");
     }
     static int getPort() {  
         if (System.getenv("PORT") != null) {
